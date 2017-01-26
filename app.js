da811ea748162e4678a1e9f702c3cb368f1de267
robsonrosa <robsonrosa@cwi.com.br>
2017-01-26 10:30:12 -0200

    Sample runner

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let constants = require('./ecmascript2016/01.constants/constants');
+let variables = require('./ecmascript2016/02.scoping/variables');
+let functions = require('./ecmascript2016/02.scoping/functions');
+let arrows = require('./ecmascript2016/03.arrow-functions/arrows');
+let defaultParameter = require('./ecmascript2016/04.parameter-handling/default-parameter');
+let restParameter = require('./ecmascript2016/04.parameter-handling/rest-parameter');
+let spread = require('./ecmascript2016/04.parameter-handling/spread');
+let stringLiteral = require('./ecmascript2016/05.literal/string-literal');
+let numberLiteral = require('./ecmascript2016/05.literal/number-literal');
+let propertyAssign = require('./ecmascript2016/06.object-properties/property-assign');
+let computedPropoertyName = require('./ecmascript2016/06.object-properties/computed-property-name');
+let methodPropoerty = require('./ecmascript2016/06.object-properties/method-property');
+
+// constants();
+// variables();
+// functions();
+// arrows();
+// defaultParameter();
+// restParameter();
+// spread();
+// stringLiteral();
+// numberLiteral();
+// propertyAssign();
+// computedPropoertyName();
+methodPropoerty();
